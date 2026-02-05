70c4da4fa43be2fa7a780ff5c154a3fbdca05dfe
Sachi <geovanasabrina@gmail.com>
2026-02-05 12:31:56 -0300

    Fatores primos de um número

--- Matematica3.py
+++ Matematica3.py
@@ -1,5 +1,5 @@
-while True:
-    numero = int(input("Digite um número: "))
+#Verifica de número é primo
+def verifica_numero(numero):
     total = 0
     contador = 1
     while contador <= numero:
@@ -8,6 +8,33 @@ while True:
         contador += 1
 
     if total == 2:
-        print("Número é primo")
+        return True
     else:
-        print("Número não é primo")
+        return False
+
+# Retorna lista de todos os primos entre 1 e número
+def listar_primos(numero):
+    lista_de_primos = []
+    for i in range(1, numero+1):
+        if verifica_numero(i) == True: 
+            lista_de_primos.append(i)
+    return lista_de_primos
+
+# Fatora um numero em números primos e mostra os números primos utilizados
+def fatorar_numero(numero):
+    #cria lista de primos desse numero
+    lista = listar_primos(numero)
+    resultado = []
+    #divide o numero pelo primeiro da lista
+    for i in lista:
+        while numero % i == 0:
+            numero = numero / i
+            resultado.append(i)
+    print(resultado)
+
+# Relevar Fatores Primos de um número.
+N = int(input("Digite um número: "))
+fatorar_numero(N)
+
+
+

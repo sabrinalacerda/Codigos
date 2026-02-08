f33bf3c65e6e9f9281188e5fd06e30611263bf98
Sachi <geovanasabrina@gmail.com>
2026-02-08 18:35:28 -0300

    Pedra, Papel, Tesoura, Lagarto e Spock

--- PedraPapeleTesoura.py
+++ PedraPapeleTesoura.py
@@ -1,15 +1,20 @@
 # Pedra, Papel e Tesoura
 # Utilizamos o resto da divisão para fechar um ciclo de opções
+while True:
+    jogador1 = int(input("Jogador1: "))
+    jogador2 = int(input("Jogador2: "))
 
-jogador1 = int(input("Pedra[0], Papel[1] ou Tesoura[2]? "))
-jogador2 = int(input("Pedra[0], Papel[1] ou Tesoura[2]? "))
+    resultado1 = (jogador2+1)%5
+    resultado2 = (jogador2+3)%5
 
-# Compara o input dos dois jogadores
-if jogador1 == jogador2:
-    print ("Empate!")
-# Ponto chave: Se jogador 2 escolher tesoura e Jogador 1 escolher pedra, diminuímos o valor de tesoura para que pedra ganhe
-# Usando resto da divisão
-elif jogador1 == (jogador2 + 1)%3:
-    print ("Jogador 1 venceu!")
-else:
-    print("Jogador 2 venceu!")
+    # Compara o input dos dois jogadores
+    if jogador1 == jogador2:
+        print ("Empate!")
+    # Ponto chave: Se jogador 2 escolher tesoura e Jogador 1 escolher pedra, diminuímos o valor de tesoura para que pedra ganhe
+    # Usando resto da divisão
+    elif jogador1 == resultado1 or jogador1 == resultado2:
+        print(jogador1, "é igual a", resultado1, "ou", resultado2)
+        print ("Jogador 1 venceu!")
+    else:
+        print(jogador1, "é diferente de", resultado1, "e", resultado2)
+        print("Jogador 2 venceu!")
